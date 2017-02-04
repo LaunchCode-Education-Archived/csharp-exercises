7cbc4c2804d78a93d74c75db11f3f1a1713891d6
Chris Bay <chris@chrisbay.net>
2017-02-04 12:11:38 -0600

    Test commit from VS Mac preview

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
