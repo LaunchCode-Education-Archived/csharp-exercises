42dd1ba9aadb527601bd39404987209f0f635980
Chris Bay <chris@chrisbay.net>
2017-02-15 11:50:29 -0600

    Add HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
