3ef801b85b04e44139633edfa0c61cf7a07293c7
Chris Bay <chris@chrisbay.net>
2017-02-15 13:08:26 -0600

    Area and Counting Characters solutions

--- Area/Program.cs
+++ Area/Program.cs
@@ -9,7 +9,15 @@ namespace Area
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.Write("Enter an radius: ");
+            string input = Console.ReadLine();
+            double radius = double.Parse(input);
+
+            double area = 3.14 * radius * radius;
+
+            Console.WriteLine("The area of circle of radius " + area + " is: " + area);
+
+            Console.ReadLine();
         }
     }
 }
