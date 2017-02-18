02151e2f40f7811b326583583a100ed2de5f5972
Chris Bay <chris@chrisbay.net>
2017-02-18 08:38:22 -0600

    Stash doesn't work on windows, so this is hacky

---
+++ GradebookDict/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradebookDict
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, double> students = new Dictionary<string, double>();
+            string newStudent;
+
+            Console.WriteLine("Enter your students:");
+            do
+            {
+                Console.Write("name: ");
+                newStudent = Console.ReadLine();
+                if (newStudent != "")
+                {
+                    // Get the student's grade
+                    Console.Write("grade: ");
+                    double aGrade = double.Parse(Console.ReadLine());
+
+                    students.Add(newStudent, aGrade);
+                }
+            }
+            while (newStudent != "");
+
+            // print roster
+            Console.WriteLine("Student roster:");
+            foreach (KeyValuePair<string, double> student in students)
+            {
+                Console.WriteLine(student.Key + " (" + student.Value + ")");
+            }
+
+            double sum = students.Values.Sum();
+            double avg = sum / students.Count;
+            Console.WriteLine("Average grade: " + avg);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMethods
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message = Message.GetMessage("fr");
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
