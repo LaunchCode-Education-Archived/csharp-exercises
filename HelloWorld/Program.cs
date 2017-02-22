1763b08be27419c5194fd76dacfb95abd33cfb59
Nicholas Cloud <me@nicholascloud.com>
2017-02-21 21:43:00 -0600

    class1 prep projects

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
