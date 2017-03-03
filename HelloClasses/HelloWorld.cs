d0bc75b411b794da4a59d5b81170a24ba8bb685f
Chris Bay <chris@chrisbay.net>
2017-03-03 10:01:27 -0600

    Partial work

---
+++ HelloClasses/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloClasses
+{
+    public class HelloWorld
+    {
+        string Message = "Hello World";
+
+        public void SayHello()
+        {
+            Console.WriteLine(Message);
+        }
+    }
+}
---
+++ HelloClasses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloClasses
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HelloWorld hello = new HelloWorld();
+            hello.SayHello();
+
+            Console.ReadLine();
+        }
+    }
+}
