2e556d6c9824910dddaa2c15480a93e6ba293e2f
Jordan M <jmcclusky17@gmail.com>
2018-02-18 17:15:03 -0600

    Finished "List" Exercise" for Class 2 Prep.

---
+++ SumOfEven/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10; i += 1)
+            {
+                numbers.Add(i + 1);
+            }
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            double sum = SumOfEven.sumOfEven(numbers);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SumOfEven/SumOfEven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumOfEven
+{
+    class SumOfEven
+    {
+        public static double sumOfEven(List<int> numbers)
+        {
+            List<int> listOfEven = new List<int>();
+            double sumOfEvens;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    listOfEven.Add(number);
+                }
+            }
+            sumOfEvens = listOfEven.Sum();
+            return sumOfEvens;
+        }
+    }
+}
