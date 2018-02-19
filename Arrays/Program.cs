c75e71599a8a3d3ee8bc01824bb07fbed2a99730
Jordan M <jmcclusky17@gmail.com>
2018-02-18 21:19:25 -0600

    Finished "Arrays" from 3.2 prep.

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 1, 2, 3, 5, 8 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
