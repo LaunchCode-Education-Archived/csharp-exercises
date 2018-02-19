ca475c58f67764d66c4c4d575909a289bd803d51
Jordan M <jmcclusky17@gmail.com>
2018-02-18 20:44:49 -0600

    Completed "Lists and Strings" from class 3.2 prep.

---
+++ ListsAndStrings/FiveLetterWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListsAndStrings
+{
+    class FiveLetterWords
+    {
+        public static List<string> fiveLetterWords(List<string> List)
+        {
+            List<string> newList = new List<string>();
+            foreach (string item in List)
+            {
+                if (item.Length == 5)
+                {
+                    newList.Add(item);
+                }
+            }
+            return newList;
+        }
+    }
+}
---
+++ ListsAndStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> testList = new List<string>();
+            testList.Add("bob");
+            testList.Add("bobby");
+            testList.Add("tom");
+            var flw = FiveLetterWords.fiveLetterWords(testList);
+            string result = "";
+            foreach (string item in flw)
+            {
+                result = result + item;
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
