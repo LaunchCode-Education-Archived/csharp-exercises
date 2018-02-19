e4612af7a4225ab63aad54f571e6c5df2d783a84
Jordan M <jmcclusky17@gmail.com>
2018-02-19 11:51:59 -0600

    Finished Counting Characters Studio class 3.2 C#

---
+++ CountingCharacters/CountCharacters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountingCharacters
+{
+    class CountCharacters
+    {
+        public static Dictionary<char, double> countCharacters(string str)
+        {
+            Dictionary<char, double> characterCount = new Dictionary<char, double>();
+            double charCount = 0;
+            foreach (char character in str)
+            {
+                charCount = str.Count(x => x ==character);
+                if (characterCount.ContainsKey(character))
+                {
+
+                }
+                else
+                {
+                characterCount.Add(character, charCount);
+                }
+            }
+            
+            return characterCount;
+        }
+    }
+}
---
+++ CountingCharacters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            foreach (KeyValuePair<char, double> item in (CountCharacters.countCharacters(testString)))
+            {
+            Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
