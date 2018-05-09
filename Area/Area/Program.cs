9f0789c4e07f392cc9c3f809e79712d9476c312d
Rim Fardiyev <threeacesstl@gmail.com>
2018-05-08 20:10:06 -0500

    Alice done

---
+++ Area/Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area, pi;
+            pi = 3.14;
+
+            
+            Console.WriteLine("Enter a radius: ");
+            radius = double.Parse(Console.ReadLine());
+            area = pi * radius * radius;
+            Console.WriteLine("The area of a circle with radius " + radius + " is " + area);
+            Console.ReadLine();
+                        
+        }
+    }
+}
