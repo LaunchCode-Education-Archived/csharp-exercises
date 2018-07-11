28391bf1ffd20683a5e0aabf7ab854a530453b92
Rim Fardiyev <threeacesstl@gmail.com>
2018-07-10 23:35:27 -0500

    Speech-to-Text artificial intelligence console application in C# using the Microsoft Bing Speech Cognitive API

---
+++ Restaurant/Restaurant/MenuItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj.GetType() != typeof(MenuItem))
+                return false;
+            return Equals((MenuItem)obj);
+        }
+
+        public bool Equals(MenuItem menuItem)
+        {
+            if (menuItem == null)
+                return false;
+            return menuItem.Name == this.Name;
+        }
+
+
+        public static void Print(MenuItem menu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHashCode(MenuItem menuItem)
+        {
+            return menuItem.Name.GetHashCode();
+        }
+
+        private DateTime dateAddedToMenu;
+        public DateTime DateAddedToMenu
+        {
+            get { return dateAddedToMenu; }
+            set { dateAddedToMenu = value; }
+        }
+    }
+}
---
+++ UserSignup/UserSignup/Models/UserData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserSignup.Models
+{
+    public class UserData
+    {
+        static private List<User> users = new List<User>();
+        
+
+        //get all
+        public static List<User> GetAll()
+        {
+            return users;
+        }
+
+
+        //Add all
+        public static void AddUser(User newUser)
+        {
+            users.Add(newUser);
+        }
+
+
+        //get user by id  
+        public static User GetUserById(int id)
+        {
+            return users.Single(x => x.UserId == id);
+        }
+
+        public static bool IsValidEmail(string email)
+        {
+            var foo = new EmailAddressAttribute();
+            bool bar;
+            bar = foo.IsValid(email);
+            return bar;
+        }
+
+        public static bool IsValidUserName(string uname)
+        {
+
+            //regex tests whether a string includes all letters
+            if ((uname.Length >= 5 && uname.Length <= 15) && (Regex.IsMatch(uname, @"^[a-zA-Z]+$")))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
