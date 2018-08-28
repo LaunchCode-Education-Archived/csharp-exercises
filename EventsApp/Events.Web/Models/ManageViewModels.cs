6173e7e88516972acbf20a950f4e89c86948823a
Rim Fardiyev <threeacesstl@gmail.com>
2018-08-28 01:16:26 -0500

    Modify the password minimum length

--- EventsApp/Events.Web/Models/ManageViewModels.cs
+++ EventsApp/Events.Web/Models/ManageViewModels.cs
@@ -28,7 +28,7 @@ namespace Events.Web.Models
     public class SetPasswordViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
@@ -47,7 +47,7 @@ namespace Events.Web.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
