a1e7d0a7e9f25418fd2533a989e64d868b89a034
Rim Fardiyev <threeacesstl@gmail.com>
2018-08-28 01:11:06 -0500

    Modify password validation policy, so it's not too complex. Delete unneeded files.

--- EventsApp/Events.Web/App_Start/IdentityConfig.cs
+++ EventsApp/Events.Web/App_Start/IdentityConfig.cs
@@ -53,11 +53,11 @@ namespace Events.Web
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 1,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
