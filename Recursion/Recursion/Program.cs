5c220f8514eb0404ba944af61cc69ad642850ad6
Rim Fardiyev <threeacesstl@gmail.com>
2018-11-01 20:37:21 -0400

    This demonstrates the appearance of the method call stack frames in a recursive algorithm.

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static int Recursive(int value, ref int count)
+        {
+            count++;
+            if (value >= 10)
+            {
+                //throw new Exception("End");
+                return value;
+            }
+            return Recursive(value + 1, ref count);
+            
+        }
+
+        static void Main()
+        {
+            int count = 0;
+            int total = Recursive(5, ref count);
+
+            Console.WriteLine(total);
+            Console.WriteLine(count);
+        }
+           
+
+    }
+}
